31cc7091420f2db9c6a89d96c52c59f1bd5724fc
rahman9868 <rahman9868>
2019-01-16 12:07:33 +0700

    Video VueJS 11

---
+++ src/Home.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>I'm Home</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
-import Message from './Message.vue';
+import Users from './Users.vue';
+import Home from './Home.vue';
 
-Vue.component('app-message',Message);
 
-// Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/users', component: Users },
+  { path: '/', component: Home }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 // /* eslint-disable no-new */
 new Vue({
   el: '#app',
+    router,
   render: h => h(App)
-})
+});
