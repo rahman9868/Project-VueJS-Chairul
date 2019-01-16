a1ecfa5e8c73b43d873efe79830b7a3ec8eca53e
rahman9868 <rahman9868@gmail.com>
2019-01-16 12:20:59 +0700

    Video VueJS 12

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
-<template>
+  <template>
   <div id="app">
-  <h1>Let's go somewhere!</h1>
-  <hr>
-  <router-view></router-view>
+      <h1>Lets Go Somewhere! </h1>
+      <hr>
+      <router-link to ="/">Home</router-link>
+      <router-link to ="/users/10">Users for Team 10</router-link>
+      <router-link to ="/users/20">Users for Team 20</router-link>
+      <hr>
+      <router-view></router-view>
   </div>
-</template>
-
-<script>
-
-
-export default {
-data () {
-  return {
-
-  }
-}
-}
-</script>
-
-<style>
-</style>
+</template
