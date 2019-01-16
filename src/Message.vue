854b5605319b98273b447a5fae54180c938834b0
rahman9868 <rahman9868@gmail.com>
2019-01-16 09:59:44 +0700

    Video VueJS 9

--- src/Message.vue
+++ src/Message.vue
@@ -1,7 +1,16 @@
 <template>
 <div>
 <h1> This Message is Great!</h1>
-
+<app-input></app-input>
 </div>
 </template>
 
+<script>
+    import Input from './Input.vue';
+
+    export default {
+      components: {
+        'app-input': Input
+      }
+    }
+</script>
